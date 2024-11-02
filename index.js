9124e8fc5bc44cedebd56f36acace2eff2a2a27b
ankurbansal15 <ankurbansal153@gmail.com>
2024-11-02 14:55:46 +0530

    Learn To create the MySQL Connection

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const { faker } = require('@faker-js/faker');
+import {faker} from '@faker-js/faker';
+
+import mysql from 'mysql2/promise';
+
+const connection = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'delta_app',
+  password: 'password'
+});
+
+try {
+    const results = await connection.query('SHOW TABLES');
+    console.log(results);
+  } catch (error) {
+    console.log(error);
+  }
+connection.end();
 
 let getRandomUser = () =>{
     return {
